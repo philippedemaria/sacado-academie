c3a120cb11a1d42b6f611218ba7e995d6b79bbf0
Philippe Demaria <philippe.demaria83@gmail.com>
2022-09-12 17:31:13 +0100

    maj lesson

--- lesson/models.py
+++ lesson/models.py
@@ -44,4 +44,24 @@ class Slot(models.Model): # disponibilité des profs
 
     def __str__(self):
         return "Dispo : prof={}, le {},  début = {}, libre ? {}".format(self.user.last_name, self.date.strftime("%d %n"), self.is_occupied)   
+
+
  
+def credit_directory_path(instance, filename):
+    return "lesson/{}/{}/{}".format(user.id, instance.id, filename)
+
+ 
+class Credit(models.Model):
+    """ Accounting   """
+
+    amount      = models.DecimalField(default=0, blank=True , max_digits=10, decimal_places=2, editable=False)# montant payé
+    effective   = models.DecimalField(default=0, blank=True , max_digits=10, decimal_places=2, editable=False)# crédit attribué
+    observation = models.TextField( blank=True, default="", null=True, verbose_name="Observation")
+    date        = models.DateTimeField(auto_now_add=True) # date de création de la facture
+    user        = models.ForeignKey(User, related_name="credits", null=True, blank=True,  on_delete=models.CASCADE, editable=False)
+    facture     = models.FileField(upload_to=credit_directory_path, blank=True, verbose_name="Facture",  default="" )
+    chrono      = models.CharField(max_length=50, blank=True, unique =True,  editable=False)
+
+
+    def __str__(self):
+        return self.user
