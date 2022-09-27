d4e6a9a68b93c9d1c8251dad8c9f35efbd542f9f
Philippe Demaria <philippe.demaria83@gmail.com>
2022-09-27 11:58:17 +0100

    Update views.py

--- association/views.py
+++ association/views.py
@@ -80,7 +80,7 @@ def transfert_asso_acad(request,idl,start):
                     old_file = ressources+'ggbfilesTMP/' + str(idl)+"/"+str(file) 
                     new_file = ressources+'ggbfiles/' + str(idl)+"/"+str(file)
                     messagers.append( str(i)+". Changement : <b>"+ str(supportfile.ggbfile) +"</b> en <b> ggbfiles/" + str(idl)+"/"+str(file)  +"</b><br/> -> Déplacement de : <b>"+ str(old_file) +"</b> vers <b>"+ str(new_file) +"</b>")               
-                    supportfile.ggbfile = file
+                    supportfile.ggbfile = 'ggbfiles/' + str(idl)+"/"+str(file)
                     i+=1
 
                     if start == 1 :
