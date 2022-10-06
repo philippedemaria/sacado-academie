fdba49b27338352ebc866809aa20593de5c6271f
Philippe Demaria <philippe.demaria83@gmail.com>
2022-10-06 10:21:08 +0100

    maj

--- staticfiles/js/script/ajax-dashboard.js
+++ staticfiles/js/script/ajax-dashboard.js
@@ -1470,7 +1470,8 @@ define(['jquery', 'bootstrap', 'ui', 'ui_sortable'], function ($) {
                             $('#average'+relationship_id).removeClass(data.remove);
                             $('#average'+relationship_id).addClass(data.add);
                             $('#card'+relationship_id).css("opacity", data.opacity);
-                            $('#publisher'+relationship_id).removeClass(data.noclass);
+                            $('#publisher'+relationship_id).removeClass(data.removeclass);
+                            $('#publisher'+relationship_id).addClass(data.addclass);
                         }
                     }
                 )
