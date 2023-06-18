90166c8d539f5e90b62331d0a51fef6f24c81748
Philippe Demaria <philippe.demaria83@gmail.com>
2023-06-18 09:09:27 +0100

    MAJ

--- holidaybook/views.py
+++ holidaybook/views.py
@@ -26,7 +26,7 @@ def try_it(request,idl):
 
 	group =  Group.objects.filter(level_id = idl,formule_id=5).last()
 	hbook = Holidaybook.objects.filter(level_id = idl, is_publish = 1).first()
-	parcours = group.group_parcours.filter(ranking=1).first()
+	parcours = group.group_parcours.filter(ranking=0).first()
 	relationships = parcours.parcours_relationship.filter(is_publish=1).order_by("ranking")
 
 	return render(request, 'holidaybook/try_it_book.html', {'parcours': parcours , 'relationships': relationships ,  'hbook' : hbook ,  'form' : form ,  'np_form' : np_form })
