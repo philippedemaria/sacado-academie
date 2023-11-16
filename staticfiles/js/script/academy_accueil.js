5fe088deb8ab4a64cdc519b721ffe1f519f16fde
Philippe Demaria <philippe.demaria83@gmail.com>
2023-11-16 17:12:28 +0100

    Update academy_accueil.js

--- staticfiles/js/script/academy_accueil.js
+++ staticfiles/js/script/academy_accueil.js
@@ -89,7 +89,7 @@ $(document).ready(function () {
             }
 
 
-        $(document).on('change', '.this_password', function (event) {
+        $(document).on('blur', '.this_password', function (event) {
 
             var value = $(this).val() ;
             if(value.length<8)
